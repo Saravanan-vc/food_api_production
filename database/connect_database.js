2922f489a99d837c677fab3cd19163734b41ee30
saravananAlabtechnology <saravanan@alabtechnology.com>
2025-01-18 17:27:33 +0530

    new

--- database/connect_database.js
+++ database/connect_database.js
@@ -1,7 +1,7 @@
 const connectDB = require('mongoose');
 require('dotenv').config();
 
-connectDB.connect(process.env.SECONDARY_MONOGODB).then(()=>{
+connectDB.connect(process.env.MONOGO_DB).then(()=>{
     console.log('created database');
 }).catch((e)=>{
     connectDB.connect(process.env.SECONDARY_MONOGODB).
